20eafcc06c95dad71e23d21a33a2d8fbc496eb08
Scott Blackwell <sblackwell@rsgc.on.ca>
2017-04-21 14:40:49 -0400

    commit.

---
+++ CanvasTestArea.playground/Sources/LindenmayerSystem.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public class LindenmayerSystem {
+    
+    // Properties (globals - available everywhere in a class)
+    var initialLength : Int     // initial line segment length
+    var currentLength : Int     // current line segment length
+    var reduction : Int         // reduction factor
+    var x : Int
+    var y : Int
+    var direction : Int
+    var angle : Degrees
+    var axiom : String
+    var rule : String
+    var n : Int
+    var word : String
+    
+    // Initializer(s)
+    public init(initialLength : Int, reduction : Int) {
+        
+        var currentLength = initialLength   // current line segment length
+        var word = axiom
+        
+        
+    }
+    
+    // Behaviours (methods)
+    
+}
